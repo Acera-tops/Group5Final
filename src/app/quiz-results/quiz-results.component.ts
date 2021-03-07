baf5a154c81f1173a32fdc01a034e8382cd7e439
april <apriltopolewski@gmail.com>
2021-03-07 14:12:44 -0500

    brainstormed questions. Took some notes as comments

--- src/app/quiz-results/quiz-results.component.ts
+++ src/app/quiz-results/quiz-results.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../recipes.service';
+import {TasteDiveService} from '../taste-dive.service'
 
 @Component({
   selector: 'app-quiz-results',
   templateUrl: './quiz-results.component.html',
   styleUrls: ['./quiz-results.component.css']
 })
-export class QuizResultsComponent implements OnInit {
+export class QuizResultsComponent implements OnInit {//functinality of component goes here
 
   constructor() { }
 
