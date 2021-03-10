fe704aecf92907739c9896ad32ade359a2542a9f
april <apriltopolewski@gmail.com>
2021-03-09 19:24:26 -0500

    added routing to links and attempted a nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {QuestionsComponent} from './questions/questions.component';
+import {QuizResultsComponent} from './quiz-results/quiz-results.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: '', component: HomeComponent},
+{path: 'Quiz', component: QuestionsComponent},
+{path: 'Results', component: QuizResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
