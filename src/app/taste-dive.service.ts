0c8e90ccf0bd7e51c94b3f2d8847dad3ea63902d
Rachel Szczembara <rachel.szczembara@perficient.com>
2021-03-17 19:04:12 -0400

    got video youtube thumbnail working

--- src/app/taste-dive.service.ts
+++ src/app/taste-dive.service.ts
@@ -1,31 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasteDiveService {
-  apiKey = "404369-AprilTop-1D5TOMF0";
-  url = "http://localhost:8080/api/similar";
+  apiKey = '404369-AprilTop-1D5TOMF0';
+  url = 'http://localhost:8080/api/similar';
   entertainmentResults: any[] = [];
   favMovies: any[] = [];
   // public movieSearch: string = "";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   getTaste(movieSearch) {
-    const requestUrl = 
-      this.getUrlWithAPIKey() + "&q=" + movieSearch; 
-      console.log(requestUrl)
+    const requestUrl = this.getUrlWithAPIKey() + '&q=' + movieSearch;
+    console.log(requestUrl);
 
     this.http.get(requestUrl).subscribe(
       (response: any) => {
-        this.entertainmentResults = response.Similar.Results;
-        console.log(this.entertainmentResults);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+        this.entertainmentResults = response.Similar.Results.map((result) => {
+          // Bypassing angular's security in order
+          // to display youtube links in an iFrame
+          // By default, angular blocks iFrames 
+          // across different domains
+          result.yUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+            result.yUrl
+          );
+
+          return result;
+        });
+      });
   }
 
   getUrlWithAPIKey() {
